1ae8cfa62fac577cb7ac6e48c9040e9e9b39f1a7
aasb2 <aasb2@cin.ufpe.br>
2019-09-22 12:07:28 -0300

    I thinq maybe q1 may be right

--- q1/q1.c
+++ q1/q1.c
@@ -1,29 +1,34 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <stdio.h>
-#define MAX_THREADS 100
+
+#define MAX_THREADS 100 //Número de thrads que serão criadas
+//"CRIA" O TIPO bool em C
 #define false 0
 #define true 1
-
 typedef char bool;
 
-long int count =0;
-bool isFinished =false;
-
+long int count =0; //Contador global
+bool isFinished =false; //variável que define quando a execução das threads irão terminar
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 void *counter(void *theadid){
 	while(true){
 		
-		if(count < 1000000)
+		if(count < 1000000){ //Soma o contatodo global até 1000000
+			pthread_mutex_lock(&mutex1);
 			count++;
-		else if(count==1000000 && !isFinished){
-			isFinished = true;
-			printf("%ld\n",count);
-			pthread_exit(NULL); 
-			
+			pthread_mutex_unlock(&mutex1);
+		}
+		else if(count==1000000 && !isFinished){ 
+			pthread_mutex_lock(&mutex1);
+			isFinished = true;	
+			pthread_mutex_unlock(&mutex1);				
+			printf("%ld\n",count);				
 		}	
-		else if(isFinished)
-			pthread_exit(NULL);
+		else
+			break;
 	}
+	pthread_exit(NULL);
 }
 
 
@@ -36,7 +41,6 @@ int main(void){
 		rc = pthread_create(&threadId[i],NULL, counter,NULL);
 		if(rc)
 			exit(-1);
-		pthread_join(threadId[i],NULL);
 	}
 	pthread_exit(NULL);
 
