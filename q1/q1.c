56a23bd8fceba053b26c4a3e639dc2f85fdbd4ca
aasb2 <aasb2@cin.ufpe.br>
2019-09-22 12:34:19 -0300

    q1 finished

--- q1/q1.c
+++ q1/q1.c
@@ -12,34 +12,32 @@ long int count =0; //Contador global
 bool isFinished =false; //variável que define quando a execução das threads irão terminar
 pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
 void *counter(void *theadid){
-	while(true){
 		
-		if(count < 1000000){ //Soma o contatodo global até 1000000
-			pthread_mutex_lock(&mutex1);
-			count++;
-			pthread_mutex_unlock(&mutex1);
-		}
-		else if(count==1000000 && !isFinished){ 
-			pthread_mutex_lock(&mutex1);
-			isFinished = true;	
-			pthread_mutex_unlock(&mutex1);				
-			printf("%ld\n",count);				
-		}	
-		else
-			break;
+	while(count < 1000000){ //Soma o contatodo global até 1000000
+		pthread_mutex_lock(&mutex1); //Não permite que duas threads acessem a mesma variável e bloqueia
+		count++;
+		pthread_mutex_unlock(&mutex1); //termina bloqueio
+		
 	}
+	pthread_mutex_lock(&mutex1); //Impede que outra thread acesse primeiro a variável isFinished
+	while(!isFinished){	
+		isFinished = true;	
+		printf("%ld\n",count);	//Printa o valor 10000000
+	}		
+	pthread_mutex_unlock(&mutex1);	//Desbloqueia a região
+
 	pthread_exit(NULL);
 }
 
 
 int main(void){
 	
-	pthread_t threadId[MAX_THREADS];
+	pthread_t threadId[MAX_THREADS]; //Cria um array de threads
 	int rc, i=0;
 
 	for(i=0; i<MAX_THREADS; i++){
-		rc = pthread_create(&threadId[i],NULL, counter,NULL);
-		if(rc)
+		rc = pthread_create(&threadId[i],NULL, counter,NULL); //tenta criar uma nova thread
+		if(rc) //em caso de erro encerra a execução
 			exit(-1);
 	}
 	pthread_exit(NULL);
