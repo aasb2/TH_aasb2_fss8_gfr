97652208aa885bda900c5c41dcfd126c4b10f9a5
Arthur Abrahao Santos Barbosa <aasb2@ug4c04.windows.cin.ufpe.br>
2019-10-03 14:27:45 -0300

    q2 part2

--- q2/q2.cpp
+++ q2/q2.cpp
@@ -1,25 +1,69 @@
 #include <bits/stdc++.h>
 #include <pthread.h>
 using namespace std;
+
+struct USER{
+	string name;
+	int id;
+	int lastAccess;
+	double score;
+	double inactivity;
+	USER(){
+		this->id = -1;
+		this->lastAccess = -1;
+		this->score = -1;
+		this->inactivity = -1;
+	}
+	USER(int name, int id, int lastAccess, double score, double inactivity){
+		this->name = name;
+		this->id = 	id;
+		this->lastAccess = lastAccess;
+		this->score = score;
+		this->inactivity = inactivity;
+	}
+};
+
+
 bool *access;
+vector<fstream > files;
+vector<vector<USER>>user;
+vector<pthread_mutex_t> mutexes;
+pthread_mutex fileMutex = PTHREAD_MUTEX_INITIALIZER;
 
-void *access_file(void* file){
+void *access_file(void* nothing){
+	bool hasFiles = true;
+	while(hasFiles){
+		int file;
+		bool isAvailable = false;
+		for(int i=0; i<A&& !isAvailable; i++){
+				pthread_mutex_lock(fileMutex);
+				if(access[i]
+				pthread_mutex_unlock(fileMutex);
+		}
+		pthread_mutex_lock()
 
-
-
+	}
+	pthread_exit(NULL);
 }
 
 int main(){
 	int N, A, T;
 	cin>>N>>A>>T;
-	vector<fstream *> files(A);
-	files accessed
-	access = new bool[A];
-	for(int i=0; i<A; i++){
-		files.at(i) = new fstream;
-		(*(files.at(i))).open("banco"+to_string(i+1)+".txt");
+	vector<pthread_t> threadid(T);
+	mutexes.resize(A);
+	for(int i =0; i<A; i++)
+		mutexes.at(i) = PTHREAD_MUTEX_INITIALIZER;
+	access = new bool[A]();
+	
+	for(int i=0; i<A; i++)
+		files.push_back(fstream("banco"+to_string(i+1)+".txt"));
+	for(int i=0; i<T; i++){
+		if(pthread_create(&threadid.at(i),NULL,access_file,NULL))
+			exit(-1);
+		pthread_join(threadid.at(i),NULL);
 	}
 	
+		
 	
 
 }
