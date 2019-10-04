26b54955640d38bd6b0a18e081dff5e1a627aa3b
Abrahão <aasb2@cin.ufpe.br>
2019-10-04 00:47:23 -0300

    I HOPE q2 works because i think i finished, but just a little mistake toke some hours to fix but I hope that this at least is right

---
+++ q6/q6.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <pthread.h>
+using namespace std;
+const int N = 4;
+int id = 0;
+vector<int> buffer;
+
+void *agendarExecução(void *arg){
+
+}
+
+void *despachante(void * arg){
+
+
+}
+
+void *pegarResultadoExecucao(void *){
+
+
+
+}
+
+
+int main(){
+
+
+
+}
