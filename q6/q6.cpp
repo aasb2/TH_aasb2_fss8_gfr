888c47f7ba9ad6575d92336f9a687f80ee301f5f
Arthur A.S Barbosa <42683500+aasb2@users.noreply.github.com>
2019-10-10 14:58:34 -0300

    Add files via upload

--- q6/q6.cpp
+++ q6/q6.cpp
@@ -32,17 +32,22 @@ const int MAX_SIZE = 1e5;
 pthread_mutex_t buffer_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t result_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t busy_mutex = PTHREAD_MUTEX_INITIALIZER;
-
+pthread_mutex_t busy_id_mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t resultCond[MAX_SIZE];
 pthread_cond_t buffer_empty = PTHREAD_COND_INITIALIZER;
 pthread_cond_t buffer_full = PTHREAD_COND_INITIALIZER;
+pthread_cond_t busy_cond = PTHREAD_COND_INITIALIZER;
+pthread_cond_t busy_id_cond = PTHREAD_COND_INITIALIZER;
 pthread_t thread[N];
 bool busy[N];
 int RESULT[MAX_SIZE];
+bool AVAILABLEIDS[MAX_SIZE];
 queue<ARGUMENT> BUFFER;
 int FULL = 0;
 int EMPTY = MAX_SIZE;
-
+int EXECUTION_THREAD =0;
+int BUSY_ID = 0;
+int AVAILABLE_ID = 0;
 
 void init(){
 	for(int i =0; i<MAX_SIZE; i++)
@@ -57,10 +62,27 @@ void *funexec(void *argument){
 		RESULT[id] = id;
 		pthread_cond_signal(&resultCond[id]);
 	pthread_mutex_unlock(&buffer_mutex);
+	pthread_mutex_lock(&busy_mutex);
+		EXECUTION_THREAD--;
+		busy[id] = false;
+	pthread_mutex_unlock(&busy_mutex);
+	pthread_cond_signal(&resultCond[id]);
+	pthread_cond_signal(&busy_cond);
 }
 
 int agendarExecucao(ARGUMENT argument){
-	int id = ID++;
+	int id;
+	pthread_mutex_lock(&busy_id_mutex);
+		while(BUSY_ID == MAX_SIZE)
+			pthread_cond_wait(&busy_cond,&busy_id_mutex);
+		for(int i =0;i<MAX_SIZE; i++){
+			if(AVAILABLEIDS[i]){
+				id =i;
+				AVAILABLEIDS[id] = false
+				break;
+			}
+		}
+	pthread_mutex_unlock(&busy_id_mutex);
 	argument.id = id;
 	pthread_mutex_lock(&buffer_mutex);
 		while(FULL == MAX_SIZE)
@@ -68,16 +90,26 @@ int agendarExecucao(ARGUMENT argument){
 		BUFFER.push(argument);
 		EMPTY--;
 		FULL++;
+		pthread_cond_signal(&buffer_empty);
 	pthread_mutex_unlock(&buffer_mutex);
 	return id;
 }
 
 int pegarResultadoExecucao(int id){
-	
+	int result;
 	pthread_mutex_lock(&result_mutex);
 		while(RESULT[id] == -1)
 			pthread_cond_wait(&resultCond[id], &result_mutex);
+		result = RESULT[id];
+		RESULT[id] = -1;
 	pthread_mutex_unlock(&result_mutex);
+	pthread_mutex_lock(&busy_id_mutex)
+		AVAILABLEIDS[id] = true;
+	pthread_mutex_unlock(&busy_id_mutex);
+	pthread_cond_signal(&busy_id_cond);
+	
+
+	return result;
 }
 
 void *despachante(void *argument){
@@ -90,20 +122,27 @@ void *despachante(void *argument){
 			BUFFER.pop();
 			EMPTY++;
 			FULL--;
+			pthread_cond_signal(&buffer_full);
 		pthread_mutex_unlock(&buffer_mutex);
 		
 		bool isBusy = true;
+
 		while(isBusy){
+			pthread_mutex_lock(&busy_mutex);
+			while(EXECUTION_THREAD == N)
+				pthread_cond_wait(&busy_mutex,&busy_cond);
 			for(int i=0; i<N && isBusy; i++){
-				pthread_mutex_lock(&busy_mutex);
 				if(!busy[i]){
+					pthread_cancel(&thread[i]);
 					pthread_create(thread[i],NULL,arg.function(),arg);
+					EXECUTION_THREAD++;
 					busy[i]= true;
 					isBusy = false;
 				}
-				pthread_mutex_unlock(&busy_mutex);
 			}
+			pthread_mutex_unlock(&busy_mutex);
 		}
+		
 	}
 	
 	
